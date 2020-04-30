3cb6c8c8b23f8d8aa239e98badcb6f3e0f245151
Vadim Kazantsev <kazantsev97vs@gmail.com>
2020-04-30 17:18:49 +0700

    App.java was modified

---
+++ src/main/java/wellink/test/task/App.java
@@ -0,0 +1,12 @@
+package wellink.test.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class);
+    }
+}
