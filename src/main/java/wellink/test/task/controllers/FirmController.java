266cb9cdccf52bf58e215bff0db1d666fb08c9f3
Vadim Kazantsev <kazantsev97vs@gmail.com>
2020-05-04 09:10:37 +0700

    FirmController.java was modified

--- src/main/java/wellink/test/task/controllers/FirmController.java
+++ src/main/java/wellink/test/task/controllers/FirmController.java
@@ -1,23 +1,32 @@
 package wellink.test.task.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import wellink.test.task.entities.materials.Materials;
 import wellink.test.task.models.ApiResponse;
 import wellink.test.task.models.reports.OrderProcessingStatisticsReport;
+import wellink.test.task.services.materials.MaterialsService;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class FirmController {
+
+    @Autowired
+    private MaterialsService materialsService;
 
     /**
      * Показать отчет статистики обработки заказов
      * @return
      */
-    @GetMapping("/statistics-report")
+    @PostMapping("/statistics-report")
     public ApiResponse<OrderProcessingStatisticsReport> showOrderProcessingStatisticsReport() {
 
-        OrderProcessingStatisticsReport report = new OrderProcessingStatisticsReport();
+        OrderProcessingStatisticsReport report = new OrderProcessingStatisticsReport(1, "a");
 
+        System.out.println(report);
         return new ApiResponse<>(
                 HttpStatus.OK.value(),
                 "All statistics",
@@ -28,7 +37,16 @@ public class FirmController {
     /**
      * предоставить количество материала
      */
-//    public ApiResponse<>
+    @PostMapping("/amount-material")
+    public ApiResponse<Materials> provideAmountMaterial() {
+        Materials materials = materialsService.get();
+
+        return new ApiResponse<>(
+                HttpStatus.OK.value(),
+                "Amount of material",
+                materials
+        );
+    }
 
 
 }
